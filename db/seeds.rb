ab8e5dcbceefea760877df8e5be6260cdfd50600
Aurelien Simon <aureliensimon@hotmail.com>
2024-01-13 12:33:27 +0100

    solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+(1..10).each do
+  completed = rand.round == 1
+  Task.create(title: Faker::Hobby.activity, details: Faker::Quote.famous_last_words, completed: completed)
+end
